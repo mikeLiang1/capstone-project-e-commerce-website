b2ee2938e1e39f272dd3c3ac55ee498c21fc05d7
GeGe1175 <GeGe1175@users.noreply.github.com>
2021-10-06 18:09:57 +1100

    added material ui button

--- frontend/src/components/buttons-and-sections/Button.js
+++ frontend/src/components/buttons-and-sections/Button.js
@@ -1,18 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import "./Button.css"
+import './Button.css';
 
-const Button = ({buttonName}) => {
-    return (
-        <button class="button">
-            {buttonName}
-        </button>
-    )
-}
+const Button = ({ buttonName }) => {
+  return <button class='button'>{buttonName}</button>;
+};
 
-export default Button
+export default Button;
 
 Button.propTypes = {
-    buttonName: PropTypes.string,
-}
+  buttonName: PropTypes.string,
+};

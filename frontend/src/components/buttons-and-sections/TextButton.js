d032f659be51f99562dc86e870c25dddcf352c4d
Jeming Zhu-Song <jemingzhu-song@Jemings-MBP.modem>
2021-10-06 21:40:42 +1100

    Customised TextButton.js

--- frontend/src/components/buttons-and-sections/TextButton.js
+++ frontend/src/components/buttons-and-sections/TextButton.js
@@ -1,30 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { Button } from '@material-ui/core';
 
 import './TextButton.css';
 
-const TextButton = ({ buttonName, buttonType="" }) => {
-
-  const theme = createTheme({
-    palette: {
-      primary: {
-        // Purple and green play nicely together.
-        main: '#000000',
-      },
-      secondary: {
-        // This is green.A700 as hex.
-        main: '#11cb5f',
-      },
-    },
-  });
+const TextButton = ({ buttonName, buttonType }) => {
   
   return (
-    <ThemeProvider theme={theme}>
-      <Button type={`${buttonType}`} color="secondary" variant='contained'>{buttonName}</Button>
-    </ThemeProvider>
+    <Button type={`${buttonType}`} style={{backgroundColor: '#000000', color: '#FFFFFF', borderRadius: '16px'}} variant="contained">{buttonName}</Button>
   );
 };
 

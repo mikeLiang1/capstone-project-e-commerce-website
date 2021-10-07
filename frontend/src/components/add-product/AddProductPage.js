b55d737dbb79fd867ed0b6f4b26c05133fb22a25
GeGe1175 <GeGe1175@users.noreply.github.com>
2021-10-07 13:17:25 +1100

    few

---
+++ frontend/src/components/add-product/AddProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddProductPage() {
+  return (
+    <div>
+      Select the Category and Sub-Category that this product belongs to: leave
+      it as None it does not belong to any Category or Sub-Category
+    </div>
+  );
+}
+
+export default AddProductPage;
--- frontend/src/components/add-products/AddProductPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AddProductPage() {
-  return <div>Product Page</div>;
-}
-
-export default AddProductPage;

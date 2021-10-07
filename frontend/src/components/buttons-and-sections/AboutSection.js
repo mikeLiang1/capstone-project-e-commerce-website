3439064dff9f9824ab0fb2ac40c72d7525d43354
Jeming <jeming.zhu-song@hotmail.com>
2021-10-07 12:32:07 +1100

    Started implementation of Home Page - Guest

---
+++ frontend/src/components/buttons-and-sections/AboutSection.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./AboutSection.css"
+
+function AboutSection() {
+    return (
+        <div class="about-section">
+            <h1>EST. 2021</h1>
+            <p><span style={{color: "#FF7A00"}}>NOCTA TECHNOLOGY</span></p>
+        </div>
+    )
+}
+
+export default AboutSection
--- frontend/src/components/home/HomePageGuest.js
+++ frontend/src/components/home/HomePageGuest.js
@@ -2,11 +2,15 @@ import React from 'react'
 
 import "./HomePageGuest.css"
 import HomePageImage from "../../images/HomePageImage.png"
+import AboutSection from '../buttons-and-sections/AboutSection'
 
 function HomeGuest() {
     return (
         <div class="home-page-guest">
-            <img src={HomePageImage} class="home-page-image"></img>
+            <div class="home-page-image-container">
+                <img src={HomePageImage} class="home-page-image"></img>
+            </div>
+            <AboutSection/>
         </div>
     )
 }

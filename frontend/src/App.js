4093f7ed200d116f54cb6537de44eb7ea6dfa84e
colinhon0 <colinhon0@gmail.com>
2021-10-10 23:18:54 +1100

    basic box page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import AddProductPage from './components/add-products/AddProductPage';
 import LoginPage from './components/user-account/LoginPage';
 import RegisterPage from './components/user-account/RegisterPage';
 import AdminDashboardPage from './components/admin-dashboard/AdminDashboardPage';
+import MysteryBoxPage from './components/mystery-box/MysteryBoxPage';
 import Footer from './components/footer/Footer';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
           <Route path='/' exact component={HomePageGuest} />
           <Route path='/login' exact component={LoginPage} />
           <Route path='/register' exact component={RegisterPage} />
+          <Route path='/mysterybox' exact component={MysteryBoxPage} />
         </Switch>
         {/* <Footer /> */}
       </Router>
---
+++ frontend/src/components/mystery-box/MysteryBoxPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import BoxGroup from './BoxGroup';
+
+import './MysteryBoxPage.css';
+
+function MysteryBoxPage() {
+  return (
+    <div className='boxPage'>
+      <div className='outline'>
+        <p>
+          <span style={{ color: '#FF7A00' }}>NOCTA TECHNOLOGY</span> offers five unique mystery boxes at different price points.<br/>
+          Each mystery box gives you the chance to win a variety of different items - only one item is “won” per mystery box.<br/>
+          Mystery boxes are priced accordingly to the probability of receiving a valuable item. <br/>
+          The prize pool for each type of mystery box is shown on the right. <br/>
+          You will have the chance to win <span style={{ color: '#FF7A00' }}>one</span> item from the prize pool.<br/>
+        </p>  
+      </div>
+      <BoxGroup/>
+    </div>
+  );
+}
+
+export default MysteryBoxPage;

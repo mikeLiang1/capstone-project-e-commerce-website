39dcc24f73c91e4aabc31355195276560d020a7a
GeGe1175 <GeGe1175@users.noreply.github.com>
2021-10-10 21:52:38 +1100

    added admin homepage

--- frontend/src/components/admin-dashboard/AdminDashboardPage.js
+++ frontend/src/components/admin-dashboard/AdminDashboardPage.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import TextButton from '../buttons-and-sections/TextButton.js';
+import { Link } from 'react-router-dom';
+
+import './AdminDashboardPage.css';
 
 function AdminDashboardPage() {
-  return <div>Admin Dashboard</div>;
+  return (
+    <div id='AdminDashboardPage-flexbox'>
+      <div>Admin Dashboard</div>
+      <div>
+        <Link to='/addproduct' style={{ textDecoration: 'none' }}>
+          <TextButton buttonName='Add Product' buttonType='button' />
+        </Link>
+      </div>
+      <div>Total Unit Sold</div>
+      <div>Total Revenue</div>
+      <div>Recent Reviews</div>
+    </div>
+  );
 }
 
 export default AdminDashboardPage;
--- frontend/src/components/navigation/NavigationBar.js
+++ frontend/src/components/navigation/NavigationBar.js
@@ -4,34 +4,66 @@ import { Link } from 'react-router-dom';
 
 import './NavigationBar.css';
 
-function NavigationBar() {
+function NavigationBar({ user }) {
   return (
-    <div className='NavigationBar'>
-      <ul className='NavigationBar-links-left'>
-        <Link to='/search' className='NavigationBar-link'>
-          <li>SEARCH</li>
-        </Link>
-        <Link to='/explore' className='NavigationBar-link'>
-          <li>EXPLORE</li>
-        </Link>
-        <Link to='/mysterybox' className='NavigationBar-link'>
-          <li>MYSTERY BOX</li>
-        </Link>
-      </ul>
-      <Link to='/' className='NavigationBar-link'>
-        <p className='NavigationBar-brand-name'>NOCTA TECH</p>
-      </Link>
-      <ul className='NavigationBar-links-right'>
-        <Link to='/login' className='NavigationBar-link'>
-          <li>LOGIN</li>
-        </Link>
-        <Link to='/register' className='NavigationBar-link'>
-          <li>REGISTER</li>
-        </Link>
-        <Link to='/cart' className='NavigationBar-link'>
-          <li>CART</li>
-        </Link>
-      </ul>
+    <div>
+      {user === 'true' ? (
+        <div className='NavigationBar'>
+          <ul className='NavigationBar-links-left'>
+            <Link to='/search' className='NavigationBar-link'>
+              <li>SEARCH</li>
+            </Link>
+            <Link to='/explore' className='NavigationBar-link'>
+              <li>EXPLORE</li>
+            </Link>
+            <Link to='/mysterybox' className='NavigationBar-link'>
+              <li>MYSTERY BOX</li>
+            </Link>
+          </ul>
+          <Link to='/' className='NavigationBar-link'>
+            <p className='NavigationBar-brand-name'>NOCTA TECH</p>
+          </Link>
+          <ul className='NavigationBar-links-right'>
+            <Link to='/login' className='NavigationBar-link'>
+              <li>SALES</li>
+            </Link>
+            <Link to='/register' className='NavigationBar-link'>
+              <li>ADD PRODUCT</li>
+            </Link>
+            <Link to='/cart' className='NavigationBar-link'>
+              <li>LOGIN/LOGOUT</li>
+            </Link>
+          </ul>
+        </div>
+      ) : (
+        <div className='NavigationBar'>
+          <ul className='NavigationBar-links-left'>
+            <Link to='/search' className='NavigationBar-link'>
+              <li>SEARCH</li>
+            </Link>
+            <Link to='/explore' className='NavigationBar-link'>
+              <li>EXPLORE</li>
+            </Link>
+            <Link to='/mysterybox' className='NavigationBar-link'>
+              <li>MYSTERY BOX</li>
+            </Link>
+          </ul>
+          <Link to='/' className='NavigationBar-link'>
+            <p className='NavigationBar-brand-name'>NOCTA TECH</p>
+          </Link>
+          <ul className='NavigationBar-links-right'>
+            <Link to='/login' className='NavigationBar-link'>
+              <li>LOGIN</li>
+            </Link>
+            <Link to='/register' className='NavigationBar-link'>
+              <li>REGISTER</li>
+            </Link>
+            <Link to='/cart' className='NavigationBar-link'>
+              <li>CART</li>
+            </Link>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }

1f8b305ce7628ac64744d27a2bb8ba920c221459
colinhon0 <colinhon0@gmail.com>
2021-10-11 14:36:14 +1100

    fixed app merge errors

--- backend/app.py
+++ backend/app.py
@@ -1,46 +1,88 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
-import pyrebase
 from firebase_admin import credentials, firestore, initialize_app
 
-from modules.HelloWorld import HelloWorld
+from modules.Register import Register
+from modules.User import User
+from modules.SignIn import SignIn
+from modules.mystery_box import *
 
 app = Flask(__name__)
 api = Api(app)
+
+
+"""
+
+    localhost:5000/product
+    
+    Ideas of functions we might need:
+        product 
+            post -> add product 
+            put -> edit product
+            get -> get product info
+            
+                database:
+                    ID PRIMARY
+                    Name
+                    Image
+                    Price
+                    Reviews [list of reviews]
+                    Description
+                    Tags
+                    Original Stock
+                    Current Stock
+                    
+                    
+        buy/product --> probably sprint 2
+        
+        auth/user 
+            put -> edit user
+            get -> get user info 
+            
+                database:
+                    email -> should only be able make one account per email PRIMARY
+                    first name
+                    last name
+                    password
+                    credit card info?
+                    address
+                    product history
+                    admin: boolean
+    
+        mysterybox
+            get -> get box info
+        
+        mysterybox/open (id)
+            get -> result
+            
+            return product id of the result
+            
+                database:
+                    mystery box id PRIMARY
+                    image
+                    price
+                    name
+                    products [(id, probablity)] list of tuples
+                            ^ links to product database
+                    
+            
+    requirements.txt
+        
+        
+"""
+
 
 # Initialize Firestore DB
 cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 
-
-class FireTest(Resource):
-    def post(self):
-        doc_ref = db.collection(u'users').document(u'alovelace')
-        doc_ref.set({
-            u'first': u'Ada',
-            u'last': u'Lovelace',
-            u'born': 1815
-        })
-        
-        return {"message" : "Success"}
-
-
-registerParser = reqparse.RequestParser()
-registerParser.add_argument('fname', type=str, help='First name required', required=True)
-registerParser.add_argument('lname', type=str, help='Last name required', required=True)
-registerParser.add_argument('email', type=str, help='Email required', required=True)
-registerParser.add_argument('password', type=str, help='Password required', required=True)
-
-class Register(Resource):
-    def post(self):
-        args = registerParser.parse_args()
-        return {"message" : args}
-        
-
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
-api.add_resource(FireTest, "/firetest")
 api.add_resource(Register, "/auth/register")
+api.add_resource(User, "/auth/user")
+api.add_resource(SignIn, "/auth/signin")
+api.add_resource(mystery_box, "/mystery_box/<string:box_name>")
+api.add_resource(mystery_box_open, "/mystery_box/<string:box_name>/open")
+
 
 if __name__ == "__main__":
     app.run(debug=True)

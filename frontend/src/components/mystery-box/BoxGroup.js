972613eeb1fcccadf0fb4084050c320cda778f53
colinhon0 <colinhon0@gmail.com>
2021-10-14 03:06:24 +1100

    mystery box fin

--- frontend/src/components/mystery-box/BoxGroup.js
+++ frontend/src/components/mystery-box/BoxGroup.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 import TextButton from '../buttons-and-sections/TextButton';
 
+import { Link } from 'react-router-dom';
+
 import './MysteryBoxPage.css';
 import SmallItemContainer from '../buttons-and-sections/SmallItemContainer';
 
@@ -12,11 +14,11 @@ function BoxGroup({ boxName }) {
   const [price, setPrice] = useState('999.99');
   const [img, setIMG] = useState('');
   const [products, setProducts] = useState([
-    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20 },
-    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20 },
-    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20 },
-    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20 },
-    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20 },
+    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20, background: 'rgba(36, 62, 206, 0.6)'},
+    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20, background: 'rgba(36, 62, 206, 0.6)' },
+    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20, background: 'rgba(36, 62, 206, 0.6)' },
+    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20, background: 'rgba(36, 62, 206, 0.6)' },
+    { itemName: '', imageUrl: '', price: '99.99', routeId: '', chance: 20, background: 'rgba(36, 62, 206, 0.6)' },
   ]);
 
   const [email, setEmail] = useState('qwewqe@qweoijqweo.com');
@@ -26,188 +28,115 @@ function BoxGroup({ boxName }) {
 
   // Function calls to the backend to retrieve name, price, image, and product ids
   async function boxRequest() {
-    // Send request to the backend
+    // Send request to the backend        
     const requestOptions = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    };
-
-    const response = await fetch('/mystery_box/' + boxName, requestOptions);
-
-    if (response.status !== 200) {
-      console.log('Not Successful');
-    } else {
-      const data = await response.json();
-      console.log('Successful');
-      console.log(data.box_data);
-      setPrice(data.box_data.Price);
-      setIMG(data.box_data.Image);
-
-      // Parse products
-
-      if (boxName === 'ultimate_box') {
-        // console.log('PRINTING FOR ' + boxName);
-        let products = [];
-        for (var ID of Object.keys(data.box_data.Products)) {
-          const productOptions = {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-          };
-
-          const response = await fetch(
-            '/mystery_box/' + boxName,
-            requestOptions
-          );
-
-          if (response.status !== 200) {
-            console.log('Not Successful');
-          } else {
-            const data = await response.json();
-            console.log('Successful');
-            console.log(data.box_data);
-            setPrice(data.box_data.Price);
-            setIMG(data.box_data.Image);
-
-            if (boxName === 'standard_box') {
-              console.log(img);
-            }
-
-            // Parse products
-
-<<<<<<< HEAD
-          products.push({
-            itemName: data.data.name,
-            imageUrl: data.data.image,
-            price: data.data.price,
-            routeId: ID,
-          });
-          console.log(data.data.image);
-=======
-            console.log('PRINTING FOR ' + boxName);
-            let products = [];
-            for (var ID of Object.keys(data.box_data.Products)) {
-              const chance = data.box_data.Products[ID];
-              console.log(`chance is ${chance}`);
-              const productOptions = {
-                method: 'GET',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-              };
-              console.log(ID);
-              console.log('so9joDSbO6U1xJsOMz7x');
-
-              const productResponse = await fetch(
-                `/product/${ID}`,
-                productOptions
-              );
-
-              //console.log("Response")
-
-              const productData = await productResponse.json();
-
-              products.push({
-                itemName: productData.data.name,
-                imageUrl: productData.data.image,
-                price: productData.data.price,
-                chance: chance,
-              });
-              console.log(productData.data);
-            }
-            setProducts(products);
-          }
->>>>>>> 43df822f154a4e0088c826a135e494bad9d6ca6d
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
         }
+      };
+      
+    const response = await fetch('/mystery_box/' + boxName, requestOptions);
+    
+  
+    if (response.status !== 200) {
+        console.log('Not Successful')
+    } else {
+      const data = await response.json()
+      console.log('Successful')
+      console.log(data.box_data)
+      setPrice(data.box_data.Price)
+      setIMG(data.box_data.Image)
+      
+      if(boxName === 'standard_box') {
+          console.log(img)
       }
-<<<<<<< HEAD
+      
+      // Parse products
+      
+      console.log("PRINTING FOR " + boxName)
+      let products = []
+      for (var ID of Object.keys(data.box_data.Products)) {
+      
+          const chance = data.box_data.Products[ID];
+          console.log(`chance is ${chance}`)
+          const productOptions = {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              }
+          }
 
-      //if data.box_data.length !== 0 {
-      // setProducts(products)
-      //}
-
-      /*
-                        const data = await response.json()
-            console.log('Successful')
-            console.log(data.box_data)
-            setPrice(data.box_data.Price)
-            setIMG(data.box_data.Image)
-
-            // Parse products
-            //console.log("Data")
-            //console.log(Object.keys(data.box_data.Products).length)
-            if (Object.keys(data.box_data.Products).length !== 0) {
-                console.log("PRINTING FOR " + boxName)
-                let products = []
-                for (var ID of Object.keys(data.box_data.Products)) {
-                    const productOptions = {
-                        method: 'GET',
-                        headers: {
-                          'Content-Type': 'application/json',
-                        }
-                    }
-                    console.log(ID)
-                    ID = ID.substring(1);
-                    console.log(ID)
-                    const productResponse = await fetch(`/product/${ID}`, productOptions)
-
-                    //console.log("Response")
-                    if (productResponse.status === 200) {
-                        const productData = await productResponse.json()
-
-                        products.push({"itemName" : productData.data.name, "imageUrl": productData.data.image, "price": productData.data.price})
-                        console.log(data.data.image)
-                    }
-                }
-
-                setProducts(products)
-            }
-            */
-=======
->>>>>>> 43df822f154a4e0088c826a135e494bad9d6ca6d
+          
+          const productResponse = await fetch(`/product/${ID}`, productOptions)
+          
+          
+          const productData = await productResponse.json()
+          
+          let background = 'rgba(36, 62, 206, 0.6)'
+          if (chance < 10) {
+            background = 'gold'
+          } else if (chance < 30) {
+            background = 'purple'
+          } else if (chance < 50) {
+            background = 'red'
+          }
+          
+          products.push({"itemName" : productData.data.name, "imageUrl": productData.data.image, "price": productData.data.price, "chance": chance, "routeId": ID, "background": background})
+          console.log(productData.data)
+      }
+      
+      function compare(a,b) {
+        if (a.chance < b.chance) {
+          return -1
+        }
+        if (a.chance < b.chance) {
+          return 1
+        }
+        
+        return 0
+      }
+      
+      products.sort(compare)
+      setProducts(products)
     }
-  }
+}
 
-  useEffect(() => {
-    boxRequest();
-    console.log('The Products are:', products);
-  }, []);
+useEffect(() => {
+    boxRequest()    
+}, [])
+
 
   // Need to route add to cart with product id
 
   return (
     <div className='boxGroup'>
       <div className='centered'>
-        <div className='outline'>
-          <b className>{title}</b>
-          <br />
-          <b>${price}</b>
-        </div>
-        <div className='boxContents'>
-          Prize Pool:
-          <div style={{ display: 'flex', overflow: 'auto' }}>
-            {products.map((item, id) => (
-              <div className='outline'>
-                <div className='container'>
-                  <SmallItemContainer
-                    key={id}
-                    itemName={item.itemName}
-                    imageUrl={item.imageUrl}
-                    productRouteId={item.routeId}
-                  ></SmallItemContainer>
-                  <div className='chance'>
-                    <p>{item.chance}%</p>
-                  </div>
-                </div>
-                <b>RRP: ${item.price}</b>
-              </div>
-            ))}
+          <div className='outline'>
+              <b className>{title}</b><br/>
+              <b>${price}</b>
           </div>
-        </div>
+          <img height="200" width="200" src= {img}/>
+          <TextButton buttonName='Add to cart' buttonType='submit' />
+      </div>
+      <div className='boxContents'>
+          Prize Pool:
+          <div style={{ display:'flex', overflow:'auto'}}>
+              {products.map((item, id) => (
+                  <div className='outline'>
+                      <div className="container">
+                          <SmallItemContainer key = {id} itemName = {item.itemName} imageUrl = {item.imageUrl} productRouteId = {item.routeId}>
+                          </SmallItemContainer>
+                          <Link to ={'/product/' + item.routeId}>
+                            <div className ="chance" style= {{ background: item.background}}><p>{item.chance}%</p></div>
+                          </Link>
+                      </div>
+                      <b>RRP: ${item.price}</b>
+                  </div>
+              ))}
+          </div>
+          
       </div>
     </div>
   );

59bea61c2dd150f3b35d03e204f573e308a19ddf
Jeming <jeming.zhu-song@hotmail.com>
2021-10-13 15:48:01 +1100

    Implemented frontend portion for Trending Products Section - can get product name and images from the backend

--- frontend/src/components/buttons-and-sections/TrendingSection.js
+++ frontend/src/components/buttons-and-sections/TrendingSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import LargeItemContainer from './LargeItemContainer';
 
@@ -18,14 +18,55 @@ function TrendingSection() {
     { id: 10, content: <LargeItemContainer /> },
   ]);
 
+  const [products, setProducts] = useState([
+    { name: '', image: '' },
+    { name: '', image: '' },
+    { name: '', image: '' },
+  ]);
+
+  const getProducts = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    };
+
+    const response = await fetch('/product/1/10', requestOptions);
+
+    if (response.status != 200) {
+      alert('Failed to get Trending Products!');
+    } else if (response.status === 200) {
+      const data = await response.json();
+      // console.log(data.products);
+      let items = [];
+      for (var i = 0; i < data.products.length; i++) {
+        // console.log(data.products[i].image);
+        // console.log(data.products[i].name);
+        items.push({
+          name: data.products[i].name,
+          image: data.products[i].image,
+        });
+      }
+      setProducts(items);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <div className='TrendingSection'>
       <div className='TrendingSection-information'>
         <h2 style={{ fontSize: '24px' }}>TRENDING</h2>
       </div>
       <div className='TrendingSection-products-section'>
-        {items.map((item) => (
-          <div key={item.id}>{item.content}</div>
+        {products.map((item, id) => (
+          <div key={id}>
+            <LargeItemContainer itemName={item.name} imageUrl={item.image} />
+          </div>
         ))}
       </div>
     </div>
--- frontend/src/components/mystery-box/BoxGroup.js
+++ frontend/src/components/mystery-box/BoxGroup.js
@@ -1,84 +1,87 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import TextButton from '../buttons-and-sections/TextButton';
 
 import './MysteryBoxPage.css';
 import SmallItemContainer from '../buttons-and-sections/SmallItemContainer';
 
-function BoxGroup({boxName}) {
-    // Dummy data
-    let title = boxName.toUpperCase()
-    title = title.replace("_", " MYSTERY ")
-    const [price, setPrice] = useState('999.99')
-    const [img, setIMG] = useState('')
-    const [products, setProducts] = useState([
-        {"itemName" : "", "imageUrl": "", "price" : "99.99"},
-        {"itemName" : "", "imageUrl": "", "price" : "99.99"},
-        {"itemName" : "", "imageUrl": "", "price" : "99.99"},
-        {"itemName" : "", "imageUrl": "", "price" : "99.99"},
-        {"itemName" : "", "imageUrl": "", "price" : "99.99"},
-    ])
-    
-    const [email, setEmail] = useState('qwewqe@qweoijqweo.com');
-    const [fname, setFirstName] = useState('oqwiejwqoie');
-    const [lname, setLastName] = useState('qweqwewq');
-    const [password, setPassword] = useState('qweqweqwewqe');
-    
-    // Function calls to the backend to retrieve name, price, image, and product ids
-    async function boxRequest() {
-        // Send request to the backend        
-        const requestOptions = {
+function BoxGroup({ boxName }) {
+  // Dummy data
+  let title = boxName.toUpperCase();
+  title = title.replace('_', ' MYSTERY ');
+  const [price, setPrice] = useState('999.99');
+  const [img, setIMG] = useState('');
+  const [products, setProducts] = useState([
+    { itemName: '', imageUrl: '', price: '99.99' },
+    { itemName: '', imageUrl: '', price: '99.99' },
+    { itemName: '', imageUrl: '', price: '99.99' },
+    { itemName: '', imageUrl: '', price: '99.99' },
+    { itemName: '', imageUrl: '', price: '99.99' },
+  ]);
+
+  const [email, setEmail] = useState('qwewqe@qweoijqweo.com');
+  const [fname, setFirstName] = useState('oqwiejwqoie');
+  const [lname, setLastName] = useState('qweqwewq');
+  const [password, setPassword] = useState('qweqweqwewqe');
+
+  // Function calls to the backend to retrieve name, price, image, and product ids
+  async function boxRequest() {
+    // Send request to the backend
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    };
+
+    const response = await fetch('/mystery_box/' + boxName, requestOptions);
+
+    if (response.status !== 200) {
+      console.log('Not Successful');
+    } else {
+      const data = await response.json();
+      console.log('Successful');
+      console.log(data.box_data);
+      setPrice(data.box_data.Price);
+      setIMG(data.box_data.Image);
+
+      // Parse products
+
+      if (boxName === 'ultimate_box') {
+        console.log('PRINTING FOR ' + boxName);
+        let products = [];
+        for (var ID of Object.keys(data.box_data.Products)) {
+          const productOptions = {
             method: 'GET',
             headers: {
               'Content-Type': 'application/json',
-              'Accept': 'application/json',
-            }
+            },
           };
-          
-        const response = await fetch('/mystery_box/' + boxName, requestOptions);
-        
-      
-        if (response.status !== 200) {
-            console.log('Not Successful')
-        } else {
-            const data = await response.json()
-            console.log('Successful')
-            console.log(data.box_data)
-            setPrice(data.box_data.Price)
-            setIMG(data.box_data.Image)
-            
-            // Parse products
-            
-            if (boxName === 'ultimate_box') {
-                console.log("PRINTING FOR " + boxName)
-                let products = []
-                for (var ID of Object.keys(data.box_data.Products)) {
-                    const productOptions = {
-                        method: 'GET',
-                        headers: {
-                          'Content-Type': 'application/json',
-                        }
-                    }
-                    console.log(ID)
-                    console.log("so9joDSbO6U1xJsOMz7x")
-                    
-                    const productResponse = await fetch(`/product/${ID}`, productOptions)
-                    
-                    //console.log("Response")
-                    
-                    const data = await productResponse.json()
-                    
-                    products.push({"itemName" : data.data.name, "imageUrl": data.data.image, "price": data.data.price})
-                    console.log(data.data.image)
-                }
-                setProducts(products)
-            }
-            
-            //if data.box_data.length !== 0 {
-            // setProducts(products)
-            //}
-            
-            /* 
+          console.log(ID);
+          console.log('so9joDSbO6U1xJsOMz7x');
+
+          const productResponse = await fetch(`/product/${ID}`, productOptions);
+
+          //console.log("Response")
+
+          const data = await productResponse.json();
+
+          products.push({
+            itemName: data.data.name,
+            imageUrl: data.data.image,
+            price: data.data.price,
+          });
+          console.log(data.data.image);
+        }
+        setProducts(products);
+      }
+
+      //if data.box_data.length !== 0 {
+      // setProducts(products)
+      //}
+
+      /* 
                         const data = await response.json()
             console.log('Successful')
             console.log(data.box_data)
@@ -115,42 +118,43 @@ function BoxGroup({boxName}) {
                 setProducts(products)
             }
             */
-        }
     }
-    
-    useEffect(() => {
-        boxRequest()    
-    }, [])
-    
+  }
 
-    
-    // Need to route add to cart with product id
-      
-    return (
-      <div className='boxGroup'>
-        <div className='centered'>
-            <div className='outline'>
-                <b className>{title}</b><br/>
-                <b>${price}</b>
-            </div>
-            <img height="200" width="200" src= {img}/>
-            <TextButton buttonName='Add to cart' buttonType='submit' />
+  useEffect(() => {
+    boxRequest();
+  }, []);
+
+  // Need to route add to cart with product id
+
+  return (
+    <div className='boxGroup'>
+      <div className='centered'>
+        <div className='outline'>
+          <b className>{title}</b>
+          <br />
+          <b>${price}</b>
         </div>
-        <div className='boxContents'>
-            Prize Pool:
-            <div style={{ display:'flex', overflow:'auto'}}>
-                {products.map((item, id) => (
-                    <div className='outline'>
-                        <SmallItemContainer key = {id} itemName = {item.itemName} imageUrl = {item.imageUrl}/>
-                        <b>RRP: ${item.price}</b>
-                    </div>
-                ))}
+        <img height='200' width='200' src={img} />
+        <TextButton buttonName='Add to cart' buttonType='submit' />
+      </div>
+      <div className='boxContents'>
+        Prize Pool:
+        <div style={{ display: 'flex', overflow: 'auto' }}>
+          {products.map((item, id) => (
+            <div className='outline'>
+              <SmallItemContainer
+                key={id}
+                itemName={item.itemName}
+                imageUrl={item.imageUrl}
+              />
+              <b>RRP: ${item.price}</b>
             </div>
-            
+          ))}
         </div>
       </div>
-    );
-  }
-  
-  export default BoxGroup;
-  
+    </div>
+  );
+}
+
+export default BoxGroup;

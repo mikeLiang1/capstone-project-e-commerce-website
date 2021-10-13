ce27980e16d913383ed1e7af5a72f275a56832e8
colinhon0 <colinhon0@gmail.com>
2021-10-13 20:17:16 +1100

    done image upload

--- frontend/src/components/add-products/AddProductPage.js
+++ frontend/src/components/add-products/AddProductPage.js
@@ -10,24 +10,61 @@ import Select from '@mui/material/Select';
 
 import './AddProductPage.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDownloadURL, getStorage, ref, uploadBytes} from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAVOqrvODx6KS-xBGs5guJTrKBJjduEjRI",
+  authDomain: "nocta-tech.firebaseapp.com",
+  projectId: "nocta-tech",
+  storageBucket: "nocta-tech.appspot.com",
+  messagingSenderId: "1002605988200",
+  appId: "1:1002605988200:web:e91efebc3765fd58b0eedd",
+  measurementId: "G-5HBFEX2BNM"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+// Get a reference to the storage service, which is used to create references in your storage bucket
+const storage = getStorage(firebaseApp);
+
+
 function AddProductPage() {
   const { register, handleSubmit } = useForm();
+  
+  const [image, setImage] = useState(null)
   const [details, setDetails] = useState({
     category: '',
     name: '',
+    image: '',
     price: '',
     tag: '',
     description: '',
   });
 
-  const onSubmit = (data) => {
-    console.log(data);
-  };
-
-  const submitData = () => {
+  const submitData = async () => {
+      
+    // Uploading image to retrieve link
+    const storageRef = ref(storage, image.name)
+      
+    let snapshot = await uploadBytes(storageRef, image)
+    
+    let url = await getDownloadURL(ref(storage, image.name))
+    
+    details.image = url
     console.log(details);
   };
-
+  
+  const handleChange = e => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0])
+    }
+  }
+  
   return (
     <div id='AddProductPage'>
       <Box sx={{ maxWidth: 130 }}>
@@ -48,10 +85,7 @@ function AddProductPage() {
       </Box>
       <div id='AddProductPage-flexbox'>
         <div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input id='file-upload' {...register('picture')} type='file' />
-            <button>Submit</button>
-          </form>
+            <input id='file-upload' onChange={handleChange} type='file' />
         </div>
         <div>
           <Box

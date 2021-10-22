62f21ede80cb7dd984622a74349277ba072ce55d
Your Name <mike.liang839@gmail.com>
2021-10-22 18:44:53 +1100

    craeted previous order page

---
+++ frontend/src/components/account/PreviousOrders.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./AccountPage.css";
+
+import TextButton from "../buttons-and-sections/TextButton";
+import RecommendMeProductsSection from "../buttons-and-sections/RecommendMeProductsSection";
+import RecommendedSection from "../buttons-and-sections/RecommendedSection";
+
+function PreviousOrders() {
+  return <div></div>;
+}
+
+export default PreviousOrders;

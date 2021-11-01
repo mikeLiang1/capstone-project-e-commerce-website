894e2017072a943194d98568958d1f00373a6903
GeGe1175 <GeGe1175@users.noreply.github.com>
2021-11-01 16:52:59 +1100

    changed credit card layout form

--- frontend/src/validateInfo.js
+++ frontend/src/validateInfo.js
@@ -14,18 +14,18 @@ export default function validateInfo(values) {
   errors.message = 'An unknown error occured. Please try again later';
   errors.cname = false;
   errors.cnumber = false;
-  errors.ctype = false;
+  //errors.ctype = false;
   errors.cexp = false;
   errors.ccvv = false;
-  errors.cpostal = false;
+  //errors.cpostal = false;
 
-  if (values.cardPostalCode === null || !values.cardPostalCode.trim()) {
-    errors.message = 'Credit card postal code is not complete';
-  } else if (creditCard.postalCode.isValid) {
-    errors.cpostal = true;
-  } else {
-    errors.message = 'Credit card postal code is invalid';
-  }
+  // if (values.cardPostalCode === null || !values.cardPostalCode.trim()) {
+  //   errors.message = 'Credit card postal code is not complete';
+  // } else if (creditCard.postalCode.isValid) {
+  //   errors.cpostal = true;
+  // } else {
+  //   errors.message = 'Credit card postal code is invalid';
+  // }
 
   //Card CVV expiration
   if (values.cardSecurityCode === null || !values.cardSecurityCode.trim()) {
@@ -46,20 +46,20 @@ export default function validateInfo(values) {
   }
 
   //Card Type Verification
-  if (
-    values.cardType === null ||
-    !values.cardType.trim() ||
-    creditCard.card === null
-  ) {
-    errors.message = 'Credit card type is not complete';
-  } else if (
-    creditCard.card.type &&
-    creditCard.card.type.toUpperCase() === values.cardType.toUpperCase()
-  ) {
-    errors.ctype = true;
-  } else {
-    errors.message = 'Credit card type is invalid';
-  }
+  // if (
+  //   values.cardType === null ||
+  //   !values.cardType.trim() ||
+  //   creditCard.card === null
+  // ) {
+  //   errors.message = 'Credit card type is not complete';
+  // } else if (
+  //   creditCard.card.type &&
+  //   creditCard.card.type.toUpperCase() === values.cardType.toUpperCase()
+  // ) {
+  //   errors.ctype = true;
+  // } else {
+  //   errors.message = 'Credit card type is invalid';
+  // }
 
   //Card Number Verification
   if (values.cardNumber === null || !values.cardNumber.trim()) {
@@ -79,14 +79,7 @@ export default function validateInfo(values) {
     errors.message = 'Cardholder name is invalid';
   }
 
-  if (
-    errors.ctype &&
-    errors.cname &&
-    errors.cnumber &&
-    errors.cexp &&
-    errors.cpostal &&
-    errors.ccvv
-  ) {
+  if (errors.cname && errors.cnumber && errors.cexp && errors.ccvv) {
     errors.variant = 'success';
     errors.message = 'Credit Card is valid';
   }

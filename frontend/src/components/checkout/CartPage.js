d5ae1eb8e3bcbcf60c5393198f7dc2bb6cd0dfe0
Jeming <jeming.zhu-song@hotmail.com>
2021-11-01 16:40:27 +1100

    Fixed issue with CheckoutPage not loading because props were not passed into that component

--- frontend/src/components/checkout/CartPage.js
+++ frontend/src/components/checkout/CartPage.js
@@ -283,7 +283,15 @@ function CartPage({ token }) {
           />
         }
       />
-      <Accordian title='Payment' content={<CheckoutPage />} />
+      <Accordian
+        title='Payment'
+        content={
+          <CheckoutPage
+            cartData={cartItems}
+            customerDetails={customerDetails}
+          />
+        }
+      />
       <Stack spacing={2} sx={{ width: '100%' }}>
         <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
           <Alert onClose={handleClose} severity='error' sx={{ width: '100%' }}>

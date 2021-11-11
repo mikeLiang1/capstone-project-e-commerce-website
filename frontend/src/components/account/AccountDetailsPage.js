8383157eb3849ad156bb822472488f74252d28f3
Jeming <jeming.zhu-song@hotmail.com>
2021-11-11 12:11:25 +1100

    Implemented account details change features: user is able to change their first name, last name and address

---
+++ frontend/src/components/account/AccountDetailsPage.js
@@ -0,0 +1,227 @@
+import React, { useEffect, useState } from 'react';
+
+import './AccountDetailsPage.css';
+import TextButton from '../buttons-and-sections/TextButton';
+import Cookies from 'js-cookie';
+import Stack from '@mui/material/Stack';
+import Snackbar from '@mui/material/Snackbar';
+import BasicTextField from '../buttons-and-sections/BasicTextField';
+import { Alert } from '@mui/material';
+
+function AccountDetailsPage() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [address, setAddress] = useState('');
+
+  const [editFirstName, setEditFirstName] = useState(false);
+  const [editLastName, setEditLastName] = useState(false);
+  const [editEmail, setEditEmail] = useState(false);
+  const [editAddress, setEditAddress] = useState(false);
+
+  const [open, setOpen] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const getCustomerDetails = async () => {
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    };
+
+    const response = await fetch(
+      `/auth/user/${Cookies.get('user')}`,
+      requestOptions
+    );
+    if (response.status !== 200) {
+      setError('Failed to get Customer Details!');
+      setOpen(true);
+    } else if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+      setFirstName(data.content.first);
+      setLastName(data.content.last);
+      setEmail(data.id);
+      setAddress(data.content.address);
+    }
+  };
+
+  const changeDetails = async () => {
+    const updatedDetails = {
+      uid: Cookies.get('user'),
+      fname: firstName,
+      lname: lastName,
+      email: email,
+      adress: address,
+    };
+
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify(updatedDetails),
+    };
+
+    const response = await fetch('/auth/user', requestOptions);
+    if (response.status === 500) {
+      console.log('Not Successful');
+    } else if (response.status === 400) {
+      alert('Backend Error');
+    } else if (response.status === 200) {
+      const data = await response.json();
+    }
+  };
+
+  useEffect(() => {
+    getCustomerDetails();
+  }, []);
+
+  return (
+    <div className='AccountDetailsPage'>
+      <h2 style={{ fontSize: '24px', paddingBottom: '32px' }}>
+        ACCOUNT DETAILS
+      </h2>
+      <div className='AccountDetailsPage-account-details'>
+        <div>
+          <div>First Name: </div>
+          {editFirstName ? (
+            <form className='AccountDetailsPage-change-details-form'>
+              <BasicTextField
+                value={firstName}
+                handleChange={(e) => setFirstName(e.target.value)}
+              />
+              <br />
+            </form>
+          ) : (
+            <div style={{ padding: '36px 0', fontWeight: '700' }}>
+              {firstName}
+            </div>
+          )}
+        </div>
+        <div>
+          {editFirstName ? (
+            <TextButton
+              buttonName='Save'
+              buttonType='submit'
+              handleClick={() => {
+                changeDetails();
+                setEditFirstName(false);
+              }}
+            />
+          ) : (
+            <TextButton
+              buttonName='Edit'
+              buttonType='button'
+              handleClick={() => {
+                setEditFirstName(true);
+              }}
+            />
+          )}
+        </div>
+      </div>
+      <div className='AccountDetailsPage-account-details'>
+        <div>
+          <div>Last Name: </div>
+          {editLastName ? (
+            <form className='AccountDetailsPage-change-details-form'>
+              <BasicTextField
+                value={lastName}
+                handleChange={(e) => setLastName(e.target.value)}
+              />
+              <br />
+            </form>
+          ) : (
+            <div style={{ padding: '36px 0', fontWeight: '700' }}>
+              {lastName}
+            </div>
+          )}
+        </div>
+        <div>
+          {editLastName ? (
+            <TextButton
+              buttonName='Save'
+              buttonType='submit'
+              handleClick={() => {
+                changeDetails();
+                setEditLastName(false);
+              }}
+            />
+          ) : (
+            <TextButton
+              buttonName='Edit'
+              buttonType='button'
+              handleClick={() => {
+                setEditLastName(true);
+              }}
+            />
+          )}
+        </div>
+      </div>
+      <div className='AccountDetailsPage-account-details'>
+        <div>
+          <div>Address: </div>
+          {editAddress ? (
+            <form className='AccountDetailsPage-change-details-form'>
+              <BasicTextField
+                value={address}
+                handleChange={(e) => setFirstName(e.target.value)}
+              />
+              <br />
+            </form>
+          ) : (
+            <div style={{ padding: '36px 0', fontWeight: '700' }}>
+              {address === null ? (
+                'No address is saved on your account'
+              ) : (
+                <div>{address}</div>
+              )}
+            </div>
+          )}
+        </div>
+        <div>
+          {editAddress ? (
+            <TextButton
+              buttonName='Save'
+              buttonType='submit'
+              handleClick={() => {
+                changeDetails();
+                setEditAddress(false);
+              }}
+            />
+          ) : (
+            <TextButton
+              buttonName='Edit'
+              buttonType='button'
+              handleClick={() => {
+                setEditAddress(true);
+              }}
+            />
+          )}
+        </div>
+      </div>
+
+      <Stack spacing={2} sx={{ width: '100%' }}>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity='error' sx={{ width: '100%' }}>
+            {error}
+          </Alert>
+        </Snackbar>
+      </Stack>
+    </div>
+  );
+}
+
+export default AccountDetailsPage;
--- frontend/src/components/account/AccountPage.js
+++ frontend/src/components/account/AccountPage.js
@@ -1,29 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./AccountPage.css";
+import './AccountPage.css';
 
-import TextButton from "../buttons-and-sections/TextButton";
-import RecommendMeProductsSection from "../buttons-and-sections/RecommendMeProductsSection";
-import RecommendedSection from "../buttons-and-sections/RecommendedSection";
+import TextButton from '../buttons-and-sections/TextButton';
+import RecommendMeProductsSection from '../buttons-and-sections/RecommendMeProductsSection';
+import RecommendedSection from '../buttons-and-sections/RecommendedSection';
 
 function AccountPage() {
   const options = ['one', 'two', 'three'];
   const defaultOption = options[0];
   return (
-    <div className="AccountPage">
-      <h2 style={{ fontSize: "24px" }}>MY ACCOUNT</h2>
-      <Link to="/previousorders" className="AccountPage-link">
-        <TextButton buttonName="Previous Orders" buttonType="button" />
+    <div className='AccountPage'>
+      <h2 style={{ fontSize: '24px' }}>MY ACCOUNT</h2>
+      <Link to='/previousorders' className='AccountPage-link'>
+        <TextButton buttonName='Previous Orders' buttonType='button' />
       </Link>
-      <Link to="/accountdetails" className="AccountPage-link">
-        <TextButton buttonName="Account Details" buttonType="button" />
+      <Link to='/accountdetails' className='AccountPage-link'>
+        <TextButton buttonName='Account Details' buttonType='button' />
       </Link>
       <RecommendMeProductsSection />
-      <div className="AccountPage-recommended">
+      <div className='AccountPage-recommended'>
         <RecommendedSection />
       </div>
-      ;
     </div>
   );
 }
